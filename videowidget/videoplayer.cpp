d72db0856cd399cd9b39e4a2454b61076df908a1
shun <shun@debian>
2013-05-30 19:11:49 +0200

    VRAIE correction qui fait que la release marche sous windows

--- videowidget/videoplayer.cpp
+++ videowidget/videoplayer.cpp
@@ -356,7 +356,7 @@ void VideoPlayer::genereASS(QString fileLyr)
   QStringList args;
   args << fileLyr;
   args << QString::number(1000.0, 'f', 6);
-  p->execute("toy2ass",args);
+  p->startDetached("./toy2ass",args);
 }
 
 void VideoPlayer::dropEvent(QDropEvent * event)
